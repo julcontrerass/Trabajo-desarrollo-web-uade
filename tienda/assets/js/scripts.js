dd91e2ba7aa09cf2a288ddebeb9f875400ec2ab2
julcontrerass <111258374+julcontrerass@users.noreply.github.com>
2023-03-07 20:35:19 -0300

    first commit

---
+++ tienda/assets/js/scripts.js
@@ -0,0 +1,88 @@
+//Julian Yago Contreras Lucero 
+jQuery('document').ready(function($){
+
+
+var menuBtn=$('.menu-icon'),
+menu=$('.navigation ul');
+
+    menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show');
+        }
+    });
+
+var carritoMenuBtn=$('.car'),
+carritomenu=$('.dropdown-menu');
+
+carritoMenuBtn.click(function(){
+        if(carritomenu.hasClass('show')){
+            carritomenu.removeClass('show');
+        }else{
+            carritomenu.addClass('show');
+        }
+    });
+
+});
+
+
+let imagenes = document.querySelectorAll('.card-img');
+let modal = document.querySelector('#modal');
+let img = document.querySelector('#modal-img');
+let boton = document.querySelector('#modal-boton');
+
+for(let i = 0; i<imagenes.length;i++){
+    imagenes[i].addEventListener('click',function(e){
+        modal.classList.toggle("modal-open");
+        let src = e.target.src;
+        img.setAttribute("src",src);
+    });
+}
+boton.addEventListener('click',function(){
+    modal.classList.toggle("modal-open");
+});
+
+
+function validarCorreo(correo){
+    var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido = expReg.test(correo);
+    if(esValido == true){
+        console.log("es valido el correo");
+    }
+    else{
+        alert("el correo no es válido");
+    }
+}
+
+function sololetras(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras = " abcdefghijklmnñopqrstuvwxyzáéíóúü";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if(key == especiales[i]){
+            teclado_especial = true; break;
+        }
+    }
+    if(letras.indexOf(teclado) == -1 && !teclado_especial){
+        return false;
+    }
+}
+
+function solonumeros(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    numeros = "0123456789";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if(key == especiales[i]){
+            teclado_especial = true; break;
+        }
+    }
+    if(numeros.indexOf(teclado) == -1 && !teclado_especial){
+        return false;
+    }
+}
